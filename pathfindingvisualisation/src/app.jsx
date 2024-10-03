81db6f59e8d79ffc73bb711308873fc9c313396c
Rémon Zander (0948619) <0948619@hr.nl>
2024-10-03 09:54:11 +0200

    added basic version of pathfinding visualisation

---
+++ pathfindingvisualisation/src/app.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState, useEffect } from 'react';
+import './tailwind.css'
+const COLUMNS = 10
+const ROWS = 10
+
+function App() 
+{
+  const [Grid, setGrid] = useState();
+  
+
+  return (
+    <div>
+      <button className="w-fit rounded-lg bg-blue-700 p-[5px] m-[10px]" onClick={() => {
+        let input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'application/json';
+        input.onchange = _ => {
+          let file = Array.from(input.files)[0]
+          console.log(file)
+        };
+        input.click()
+      }}>
+        LoadFile
+      </button>
+
+      <button onClick={() => {setGrid(MakeGrid())}}>MakeGrid</button>
+      {Grid == null ? null : <div className="flex gap-x-[5px] border-2 border-solid border-black w-fit">{Grid}</div>}
+      
+    </div>
+  )
+}
+
+function MakeGrid()
+{
+  let content = []
+  for (let i = 0; i < COLUMNS; i++) {
+    let rows = []
+    for (let j = 0; j < ROWS; j++) {
+      rows.push(<div className="min-w-[20px] min-h-[20px] border-2 border-solid border-black"></div>)
+    }
+    content.push(<div className="min-w-[30px] pt-[5px] pb-[5px] flex flex-col items-center gap-y-[5px]">{rows}</div>)
+  }
+  return content
+}
+
+export default App;
