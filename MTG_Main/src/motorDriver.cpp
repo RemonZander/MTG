e082f67ed7dc254942eb5cba7d60821f2ebc01c9
FReenen <git@finnvanreenen.nl>
2024-10-17 15:54:45 +0200

    motionController: inital implementation

---
+++ MTG_Main/src/motorDriver.cpp
@@ -0,0 +1,69 @@
+#include "motorDriver.hpp"
+#include <cstdint>
+
+MotorDriver::MotorDriver(motorPins_t pinsMotorA, motorPins_t pinsMotorB, uint32_t endStopXPin, uint32_t endStopYPin)
+    : endStopXPin(endStopXPin), endStopYPin(endStopYPin)
+{
+    stepperMotorA = new AccelStepper(1, pinsMotorA.step, pinsMotorA.dir);
+    stepperMotorB = new AccelStepper(1, pinsMotorB.step, pinsMotorB.dir);
+
+    pinMode(endStopXPin, INPUT);
+    pinMode(endStopYPin, INPUT);
+}
+
+void MotorDriver::setSpeeds(uint32_t maxSpeed, uint32_t acceleration, uint32_t jurk)
+{
+	stepperMotorA->setMaxSpeed(maxSpeed);
+	stepperMotorA->setAcceleration(acceleration);
+
+	stepperMotorB->setMaxSpeed(maxSpeed);
+	stepperMotorB->setAcceleration(acceleration);
+
+    this->jurk = jurk;
+}
+
+void MotorDriver::move(int32_t deltaA, int32_t deltaB, uint32_t speed)
+{
+	stepperMotorA->setSpeed(speed);
+	stepperMotorB->setSpeed(speed);
+
+	stepperMotorA->moveTo(deltaA);
+	stepperMotorB->moveTo(deltaB);
+
+    bool motorAFinished = false, motorBFinished = false;
+
+    while (!motorAFinished && !motorAFinished)
+    {
+        motorAFinished = stepperMotorA->run();
+        motorBFinished = stepperMotorB->run();
+    }
+}
+
+void MotorDriver::home(int32_t maxMove, uint32_t speed)
+{
+	stepperMotorA->setSpeed(speed);
+	stepperMotorB->setSpeed(speed);
+
+	stepperMotorA->moveTo(maxMove);
+	stepperMotorB->moveTo(-maxMove);
+
+    bool motorAFinished = false, motorBFinished = false;
+
+    while (!motorAFinished && !motorAFinished && (digitalRead(endStopXPin) == 1))
+    {
+        motorAFinished = stepperMotorA->run();
+        motorBFinished = stepperMotorB->run();
+    }
+
+	stepperMotorA->moveTo(maxMove);
+	stepperMotorB->moveTo(-maxMove);
+
+    motorAFinished = false;
+    motorBFinished = false;
+
+    while (!motorAFinished && !motorAFinished && (digitalRead(endStopYPin) == 1))
+    {
+        motorAFinished = stepperMotorA->run();
+        motorBFinished = stepperMotorB->run();
+    }
+}
