cf9dc95f73df0691be81d3c73847762929dc90d1
FReenen <git@finnvanreenen.nl>
2024-12-05 16:41:34 +0100

    first problebly (not) working motordriver

--- MTG_Main/main/motorDriver.idf.cpp
+++ MTG_Main/main/motorDriver.idf.cpp
@@ -7,7 +7,10 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
+#define CURVE_SAPLES_COUND 500
+
 typedef struct {
+    rmt_encoder_t base;
     rmt_encoder_handle_t copy_encoder;
     uint32_t sample_points;
     struct {
@@ -22,8 +25,28 @@ typedef struct {
     uint32_t resolution;
 } rmt_stepper_uniform_encoder_t;
 
+rmt_channel_handle_t motor_chan = NULL;
 rmt_stepper_curve_encoder_t *accel_curve = NULL;
+rmt_stepper_uniform_encoder_t *constant_curve = NULL;
 rmt_stepper_curve_encoder_t *decel_curve = NULL;
+
+static size_t rmt_encode_stepper_motor_curve(rmt_encoder_t *encoder, rmt_channel_handle_t channel, const void *primary_data, size_t data_size, rmt_encode_state_t *ret_state)
+{
+    rmt_stepper_curve_encoder_t *motor_encoder = __containerof(encoder, rmt_stepper_curve_encoder_t, base);
+    rmt_encoder_handle_t copy_encoder = motor_encoder->copy_encoder;
+    rmt_encode_state_t session_state = RMT_ENCODING_RESET;
+    uint32_t sample_points = *(uint32_t *)primary_data;
+    size_t encoded_symbols = 0;
+    if (motor_encoder->flags.is_accel_curve) {
+        encoded_symbols = copy_encoder->encode(copy_encoder, channel, &motor_encoder->curve_table[0],
+                                               sample_points * sizeof(rmt_symbol_word_t), &session_state);
+    } else {
+        encoded_symbols = copy_encoder->encode(copy_encoder, channel, &motor_encoder->curve_table[0] + motor_encoder->sample_points - sample_points,
+                                               sample_points * sizeof(rmt_symbol_word_t), &session_state);
+    }
+    *ret_state = session_state;
+    return encoded_symbols;
+}
 
 static float convert_to_smooth_freq(uint32_t freq1, uint32_t freq2, uint32_t freqx)
 {
@@ -51,7 +74,6 @@ MotorDriver::MotorDriver(motorPins_t pinsMotorA, motorPins_t pinsMotorB, uint32_
     ESP_ERROR_CHECK(gpio_config(&en_dir_gpio_config));
 
     ESP_LOGI(TAG, "Create RMT TX channel");
-    rmt_channel_handle_t motor_chan = NULL;
     rmt_tx_channel_config_t tx_chan_config = {
         .clk_src = RMT_CLK_SRC_DEFAULT, // select clock source
         .gpio_num = pinsMotorA.step,
@@ -80,16 +102,17 @@ MotorDriver::~MotorDriver()
 
 void MotorDriver::SetSpeeds(uint32_t maxSpeed, uint32_t acceleration, uint32_t jurk)
 {
+    //TODO: speeds are in mm/sec need to be converted to steps/sec, but this can be diffrent per motor
     // generate accelaration curve
     if (accel_curve != NULL)
     {
         free(accel_curve);
     }
-    accel_curve = rmt_alloc_encoder_mem(sizeof(rmt_stepper_curve_encoder_t) + config->sample_points * sizeof(rmt_symbol_word_t));
-    float curve_step = (maxSpeed - jurk) / (config->sample_points - 1);
-    accel_curve->sample_points = config->sample_points;
+    accel_curve = rmt_alloc_encoder_mem(sizeof(rmt_stepper_curve_encoder_t) + CURVE_SAPLES_COUND * sizeof(rmt_symbol_word_t));
+    float curve_step = (maxSpeed - jurk) / (CURVE_SAPLES_COUND - 1);
+    accel_curve->sample_points = CURVE_SAPLES_COUND;
     accel_curve->flags.is_accel_curve = true;
-    for (uint32_t i = 0; i < config->sample_points; i++)
+    for (uint32_t i = 0; i < CURVE_SAPLES_COUND; i++)
     {
         smooth_freq = convert_to_smooth_freq(jurk, maxSpeed, jurk + curve_step * i);
         symbol_duration = config->resolution / smooth_freq / 2;
@@ -99,23 +122,31 @@ void MotorDriver::SetSpeeds(uint32_t maxSpeed, uint32_t acceleration, uint32_t j
         accel_curve->curve_table[i].duration1 = symbol_duration;
     }
 
+    // generate constsnt curve
+    if (constant_curve != NULL)
+    {
+        free(constant_curve);
+    }
+    constant_curve = rmt_alloc_encoder_mem(sizeof(rmt_stepper_uniform_encoder_t));
+    constant_curve->resolution = CURVE_SAPLES_COUND;
+
     // generate decalaration curve
     if (decel_curve != NULL)
     {
         free(decel_curve);
     }
-    decel_curve = rmt_alloc_encoder_mem(sizeof(rmt_stepper_curve_encoder_t) + config->sample_points * sizeof(rmt_symbol_word_t));
-    float curve_step = (jurk - maxSpeed) / (config->sample_points - 1);
+    decel_curve = rmt_alloc_encoder_mem(sizeof(rmt_stepper_curve_encoder_t) + CURVE_SAPLES_COUND * sizeof(rmt_symbol_word_t));
+    float curve_step = (jurk - maxSpeed) / (CURVE_SAPLES_COUND - 1);
     decel_curve->flags.is_accel_curve = false;
-    decel_curve->sample_points = config->sample_points;
-    for (uint32_t i = 0; i < config->sample_points; i++)
+    decel_curve->sample_points = CURVE_SAPLES_COUND;
+    for (uint32_t i = 0; i < CURVE_SAPLES_COUND; i++)
     {
         smooth_freq = convert_to_smooth_freq(maxSpeed, jurk, maxSpeed + curve_step * i);
         symbol_duration = config->resolution / smooth_freq / 2;
-        decel_curve->curve_table[config->sample_points - i - 1].level0 = 0;
-        decel_curve->curve_table[config->sample_points - i - 1].duration0 = symbol_duration;
-        decel_curve->curve_table[config->sample_points - i - 1].level1 = 1;
-        decel_curve->curve_table[config->sample_points - i - 1].duration1 = symbol_duration;
+        decel_curve->curve_table[CURVE_SAPLES_COUND - i - 1].level0 = 0;
+        decel_curve->curve_table[CURVE_SAPLES_COUND - i - 1].duration0 = symbol_duration;
+        decel_curve->curve_table[CURVE_SAPLES_COUND - i - 1].level1 = 1;
+        decel_curve->curve_table[CURVE_SAPLES_COUND - i - 1].duration1 = symbol_duration;
     }
 }
 
@@ -127,148 +158,94 @@ void MotorDriver::SetStepsPerMM(int32_t a, int32_t b)
 
 void MotorDriver::move(float x, float y, uint32_t speed)
 {
-    int32_t stepsA = -y + x;
-    int32_t stepsB = -y - x;
-	stepperMotorA->move(stepsA * stepsPerMMA);
-	stepperMotorB->move(stepsB * stepsPerMMB);
+    int32_t stepsA = (-y + x) * stepsPerMMA;
+    int32_t stepsB = (-y - x) * stepsPerMMB;
 
-    bool motorAFinished = false, motorBFinished = false;
+    gpio_set_level(pinsMotorA.dir, (stepsA) ? STEPER_DIR_CW : STEPER_DIR_CCW);
+    gpio_set_level(pinsMotorB.dir, (stepsB) ? STEPER_DIR_CW : STEPER_DIR_CCW);
 
-    while (!motorAFinished || !motorBFinished)
+    rmt_transmit_config_t tx_config = {
+        .loop_count = 0,
+    };
+    uint32_t curve_samples;
+
+    // stepper A
+    if (stepsA < CURVE_SAPLES_COUND * 2)
     {
-        stepperMotorA->run();
-        stepperMotorB->run();
-        motorAFinished = stepperMotorA->distanceToGo() == 0;
-        motorBFinished = stepperMotorB->distanceToGo() == 0;
+        // acceleraton
+        curve_samples = stepsA / 2;
+        rmt_transmit(motor_chan, &accel_curve->base, &curve_samples, sizeof(curve_samples), &tx_config);
+        // decelaraton
+        curve_samples += stepsA & 0x1; // add one if odd
+        rmt_transmit(motor_chan, &decel_curve->base, &curve_samples, sizeof(curve_samples), &tx_config);
+    }
+    else
+    {
+        // acceleraton
+        curve_samples = CURVE_SAPLES_COUND;
+        rmt_transmit(motor_chan, &accel_curve->base, &CURVE_SAPLES_COUND, sizeof(CURVE_SAPLES_COUND), &tx_config);
+        // constant speed
+        tx_config.loop_count = stepsA - CURVE_SAPLES_COUND*2;
+        rmt_transmit(motor_chan, &constant_curve->base, &curve_samples, sizeof(curve_samples), &tx_config);
+        // decelaraton
+        tx_config.loop_count = 0;
+        rmt_transmit(motor_chan, &decel_curve->base, &CURVE_SAPLES_COUND, sizeof(CURVE_SAPLES_COUND), &tx_config);
+    }
+
+    // stepper B
+    if (stepsB < CURVE_SAPLES_COUND * 2)
+    {
+        // acceleraton
+        curve_samples = stepsB / 2;
+        rmt_transmit(motor_chan, &accel_curve->base, &curve_samples, sizeof(curve_samples), &tx_config);
+        // decelaraton
+        curve_samples += stepsB & 0x1; // add one if odd
+        rmt_transmit(motor_chan, &decel_curve->base, &curve_samples, sizeof(curve_samples), &tx_config);
+    }
+    else
+    {
+        // acceleraton
+        curve_samples = CURVE_SAPLES_COUND;
+        rmt_transmit(motor_chan, &accel_curve->base, &CURVE_SAPLES_COUND, sizeof(CURVE_SAPLES_COUND), &tx_config);
+        // constant speed
+        tx_config.loop_count = stepsB - CURVE_SAPLES_COUND*2;
+        rmt_transmit(motor_chan, &constant_curve->base, &curve_samples, sizeof(curve_samples), &tx_config);
+        // decelaraton
+        tx_config.loop_count = 0;
+        rmt_transmit(motor_chan, &decel_curve->base, &CURVE_SAPLES_COUND, sizeof(CURVE_SAPLES_COUND), &tx_config);
     }
 }
 
 void MotorDriver::home(int32_t maxMove, uint32_t speed, float offsetX, float offsetY)
 {
-	stepperMotorA->move(-maxMove);
-	stepperMotorB->move(maxMove);
-
-    bool motorAFinished = false, motorBFinished = false;
-
-    while (!motorAFinished && !motorBFinished && (digitalRead(endStopXPin) == 1))
-    {
-        stepperMotorA->run();
-        stepperMotorB->run();
-        motorAFinished = stepperMotorA->distanceToGo() == 0;
-        motorBFinished = stepperMotorB->distanceToGo() == 0;
-    }
-    stepperMotorA->stop();
-    stepperMotorB->stop();
-
-	stepperMotorA->move(maxMove);
-	stepperMotorB->move(maxMove);
-
-    motorAFinished = false;
-    motorBFinished = false;
-
-    while (!motorAFinished && !motorBFinished && (digitalRead(endStopYPin) == 0))
-    {
-        stepperMotorA->run();
-        stepperMotorB->run();
-        motorAFinished = stepperMotorA->distanceToGo() == 0;
-        motorBFinished = stepperMotorB->distanceToGo() == 0;
-    }
-    stepperMotorA->stop();
-    stepperMotorB->stop();
-
-	float deltaA = -offsetY + offsetX;
-	float deltaB = -offsetY - offsetX;
-	stepperMotorA->move((long) deltaA * 10);
-	stepperMotorB->move((long) deltaB * 10);
-
-    motorAFinished = false;
-    motorBFinished = false;
-
-    while (!motorAFinished || !motorBFinished)
-    {
-        stepperMotorA->run();
-        stepperMotorB->run();
-        motorAFinished = stepperMotorA->distanceToGo() == 0;
-        motorBFinished = stepperMotorB->distanceToGo() == 0;
-    }
-    stepperMotorA->stop();
-    stepperMotorB->stop();
-}
-
-
-/*
- * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-
-
-///////////////////////////////Change the following configurations according to your board//////////////////////////////
-#define STEP_MOTOR_GPIO_EN       0
-#define STEP_MOTOR_GPIO_DIR      2
-#define STEP_MOTOR_GPIO_STEP     4
-#define STEP_MOTOR_ENABLE_LEVEL  0 // DRV8825 is enabled on low level
-#define STEP_MOTOR_SPIN_DIR_CLOCKWISE 0
-#define STEP_MOTOR_SPIN_DIR_COUNTERCLOCKWISE !STEP_MOTOR_SPIN_DIR_CLOCKWISE
-
-#define STEP_MOTOR_RESOLUTION_HZ 1000000 // 1MHz resolution
-
-static const char *TAG = "example";
-
-void app_main(void)
-{
-    ESP_LOGI(TAG, "Create motor encoders");
-    stepper_motor_curve_encoder_config_t accel_encoder_config = {
-        .resolution = STEP_MOTOR_RESOLUTION_HZ,
-        .sample_points = 500,
-        .start_freq_hz = 500,
-        .end_freq_hz = 1500,
-    };
-    rmt_encoder_handle_t accel_motor_encoder = NULL;
-    ESP_ERROR_CHECK(rmt_new_stepper_motor_curve_encoder(&accel_encoder_config, &accel_motor_encoder));
-
-    stepper_motor_uniform_encoder_config_t uniform_encoder_config = {
-        .resolution = STEP_MOTOR_RESOLUTION_HZ,
-    };
-    rmt_encoder_handle_t uniform_motor_encoder = NULL;
-    ESP_ERROR_CHECK(rmt_new_stepper_motor_uniform_encoder(&uniform_encoder_config, &uniform_motor_encoder));
-
-    stepper_motor_curve_encoder_config_t decel_encoder_config = {
-        .resolution = STEP_MOTOR_RESOLUTION_HZ,
-        .sample_points = 500,
-        .start_freq_hz = 1500,
-        .end_freq_hz = 500,
-    };
-    rmt_encoder_handle_t decel_motor_encoder = NULL;
-    ESP_ERROR_CHECK(rmt_new_stepper_motor_curve_encoder(&decel_encoder_config, &decel_motor_encoder));
-
-    ESP_LOGI(TAG, "Enable RMT channel");
-    ESP_ERROR_CHECK(rmt_enable(motor_chan));
-
-    ESP_LOGI(TAG, "Spin motor for 6000 steps: 500 accel + 5000 uniform + 500 decel");
     rmt_transmit_config_t tx_config = {
-        .loop_count = 0,
+        .loop_count = maxMove,
     };
 
-    const static uint32_t accel_samples = 500;
-    const static uint32_t uniform_speed_hz = 1500;
-    const static uint32_t decel_samples = 500;
+    gpio_set_level(pinsMotorA.dir, STEPER_DIR_CCW);
+    gpio_set_level(pinsMotorB.dir, STEPER_DIR_CW);
 
-    while (1) {
-        // acceleration phase
-        tx_config.loop_count = 0;
-        ESP_ERROR_CHECK(rmt_transmit(motor_chan, accel_motor_encoder, &accel_samples, sizeof(accel_samples), &tx_config));
+    rmt_transmit(motor_chan, &constant_curve->base, &speed, sizeof(uint32_t), &tx_config);
 
-        // uniform phase
-        tx_config.loop_count = 5000;
-        ESP_ERROR_CHECK(rmt_transmit(motor_chan, uniform_motor_encoder, &uniform_speed_hz, sizeof(uniform_speed_hz), &tx_config));
-
-        // deceleration phase
-        tx_config.loop_count = 0;
-        ESP_ERROR_CHECK(rmt_transmit(motor_chan, decel_motor_encoder, &decel_samples, sizeof(decel_samples), &tx_config));
-        // wait all transactions finished
-        ESP_ERROR_CHECK(rmt_tx_wait_all_done(motor_chan, -1));
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
+    //TODO: check if motors are finished
+    while (true && (digitalRead(endStopXPin) == 1))
+    {
+        // do nothing?
     }
+    //TODO: stop motors
+
+    gpio_set_level(pinsMotorA.dir, STEPER_DIR_CW);
+    gpio_set_level(pinsMotorB.dir, STEPER_DIR_CW);
+
+    rmt_transmit(motor_chan, &constant_curve->base, &speed, sizeof(uint32_t), &tx_config);
+
+
+    while (true && (digitalRead(endStopYPin) == 0))
+    {
+        // do nothing?
+    }
+    //TODO: stop motors
+
+    // move to center of (0,0)
+    move(offsetX, offsetY, jurk);
 }
