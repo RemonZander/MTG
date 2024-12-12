61dc50e3ea7dfa0b2bc151663339be6ab59fc9d0
FReenen <git@finnvanreenen.nl>
2024-12-12 11:41:42 +0100

    fix bug

--- MTG_Main/main/config.h
+++ MTG_Main/main/config.h
@@ -1,8 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include "typedefs.h"
-
 #define LOGLEVEL_DEBUG 5
 #define LOGLEVEL_INFO 4
 #define LOGLEVEL_WARN 3
--- MTG_Main/main/typedefs.h
+++ MTG_Main/main/typedefs.h
@@ -29,7 +29,7 @@ typedef struct Cordinates_s {
 
 typedef struct BoardMap_s {
     Coordinates_t size;
-    uint8_t map[BOARD_SIZE_X_LUDO][BOARD_SIZE_Y_LUDO];
+    uint8_t map[BOARD_SIZE_X_MAX][BOARD_SIZE_Y_MAX];
 } BoardMap_t;
 
 struct pathfinding_step_t {
