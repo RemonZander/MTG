93a2c19d5c5732e9941907744a31b86f2fe6ea8d
remon zander <remon.zander1@gmail.com>
2024-12-19 13:55:38 +0100

    changed log

--- MTG_Main/test/LudoGame/test.cpp
+++ MTG_Main/test/LudoGame/test.cpp
@@ -1,12 +1,14 @@
 #include <gtest/gtest.h>
 #include "LudoGame.hpp"
 #include "../../main/logger.h"
+#include <stdint.h>
+#include <stdio.h>
 
 TEST(LudoGame, init)
 {
-    LOG_I("initializing test");
+    printf("initializing test");
     LudoGame *game = new LudoGame();
-    LOG_I("created game object");
+    printf("created game object");
     game->Init();
     game->GameLoop();
     delete(game);
