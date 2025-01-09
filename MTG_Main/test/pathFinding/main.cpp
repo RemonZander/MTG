02085b722dcd38324f6ef9c6b1ddddd19fd6fe4f
remon zander <remon.zander1@gmail.com>
2025-01-09 13:54:38 +0100

    did a few tests with pathfinding. Some tweaks to the weightmap and some other fixes

--- MTG_Main/test/pathFinding/main.cpp
+++ MTG_Main/test/pathFinding/main.cpp
@@ -1,5 +1,7 @@
 #include "../../main/PathFinding.hpp"
 #include "../../main/typedefs.h"
+#include "../../main/Pawns.hpp"
+#include "../../main/ludoStates.hpp"
 
 #include <stdio.h>
 #include <stddef.h>
@@ -7,35 +9,39 @@
 
 int main(void)
 {
-    PathFinding_impoved pathFind = PathFinding_impoved();
+    PathFinding_impoved *pathFind = new PathFinding_impoved();
 
     BoardMap_t wightMap = {
-        .size = {.x = 10, .y = 10},
+        .size = {.x = BOARD_SIZE_X_LUDO, .y = BOARD_SIZE_Y_LUDO},
         .map = {
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+            {255, 255, 255, 255, 255,  10,   2,   2,   2,  10, 255, 255, 255, 255, 255},
+            {255, 255,   1, 255,  20,  10,   2,  30,   2,  10,  20, 255,   1, 255, 255},
+            {255,   1,   1,   1,  20,  10,   2,  30,   2,  10,  20,   1,   1,   1, 255},
+            {255, 255,   1, 255,  20,  10,   2,  30,   2,  10,  20, 255,   1, 255, 255},
+            {255,  20,  20,  20,  20,  10,   2,  30,   2,  10,  20,  20,  20,  20, 255},
+            { 10,  10,  10,  10,  10,  10,   2,  30,   2,  10,  10,  10,  10,  10,  10},
+            {  2,   2,   2,   2,   2,   2,   2, 255,   2,   2,   2,   2,   2,   2,   2},
+            {  2,  30,  30,  30,  30,  30, 255, 255, 255,  30,  30,  30,  30,  30,   2},
+            {  2,   2,   2,   2,   2,   2,   2, 255,   2,   2,   2,   2,   2,   2,   2},
+            { 10,  10,  10,  10,  10,  10,   2,  30,   2,  10,  10,  10,  10,  10,  10},
+            {255,  20,  20,  20,  20,  10,   2,  30,   2,  10,  20,  20,  20,  20, 255},
+            {255, 255,   1, 255,  20,  10,   2,  30,   2,  10,  20, 255,   1, 255, 255},
+            {255,   1,   1,   1,  20,  10,   2,  30,   2,  10,  20,   1,   1,   1, 255},
+            {255, 255,   1, 255,  20,  10,   2,  30,   2,  10,  20, 255,   1, 255, 255},
+            {255, 255, 255, 255, 255,  10,   2,   2,   2,  10, 255, 255, 255, 255, 255}
         }
     };
-    Cordinates_s start = { .x = 2, .y = 2 };
-    Cordinates_s end = { .x = 7, .y = 7 };
-    std::vector<Cordinates_s> *powns = new std::vector<Cordinates_s>;
-    powns->push_back({.x = 1, .y = 7});
-    powns->push_back({.x = 2, .y = 6});
-    powns->push_back({.x = 3, .y = 5});
-    // powns->push_back({.x = 4, .y = 4});
-    powns->push_back({.x = 5, .y = 3});
-    powns->push_back({.x = 6, .y = 2});
-    powns->push_back({.x = 7, .y = 1});
 
-    pathfinding_path_t path = pathFind.findPath(start, end, wightMap, powns, 5);
+
+    Cordinates_s start = { .x = 13, .y = 1 };
+    Cordinates_s end = { .x = 13, .y = 6 };
+    std::vector<Pawn<LudoPawnState_t>*>* pawns = new std::vector<Pawn<LudoPawnState_t>*>{
+        new Pawn<LudoPawnState_t>(0, Coordinates_t{9, 6}, LudoPawnState_t{.IsSelected = false, .Steps = 0, .IsAtStart = false, .IsInHome = false, .HasFinished = false, .startPos = Coordinates_t{9, 6}}),
+        new Pawn<LudoPawnState_t>(0, Coordinates_t{8, 1}, LudoPawnState_t{.IsSelected = false, .Steps = 0, .IsAtStart = false, .IsInHome = false, .HasFinished = false, .startPos = Coordinates_t{8, 1}}),
+        new Pawn<LudoPawnState_t>(0, Coordinates_t{3, 5}, LudoPawnState_t{.IsSelected = false, .Steps = 0, .IsAtStart = false, .IsInHome = false, .HasFinished = false, .startPos = Coordinates_t{3, 5}})
+    };
+
+    pathfinding_path_t path = pathFind->findPath(start, end, wightMap, pawns, 255);
     delete(path);
 }
 
