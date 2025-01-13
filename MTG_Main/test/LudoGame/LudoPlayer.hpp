7fa21e431d65aa2e2195ee6d645e489c0909e838
remon zander <remon.zander1@gmail.com>
2025-01-13 12:39:04 +0100

    maybe fixed gamelogic

--- MTG_Main/test/LudoGame/LudoPlayer.hpp
+++ MTG_Main/test/LudoGame/LudoPlayer.hpp
@@ -13,16 +13,23 @@ class LudoPlayer : public Player<T, U> {
         void DoTurn()
         {
             LOG_I("Player %u turn has started", this->ID);
-            uint8_t diceroll;
+            uint8_t diceroll = Random::get<uint8_t>(1, 6) + Random::get<uint8_t>(1, 6);
+            LOG_I("Player %u rolls: %u", this->ID, diceroll);
 
-            uint8_t selectedPawn;
+            int selectedPawn = -1;
 
+            //can player do anything. if not return
+            if (diceroll != 6 && !this->State.HasPawnOnboard) return;
+
+            //can player do anything. if not return
+            if (diceroll != 6 && ((*this->Pawns)[0]->State.HasFinished || (*this->Pawns)[0]->State.IsAtStart) && 
+            ((*this->Pawns)[1]->State.HasFinished || (*this->Pawns)[1]->State.IsAtStart) &&
+            ((*this->Pawns)[2]->State.HasFinished || (*this->Pawns)[2]->State.IsAtStart) &&
+            ((*this->Pawns)[3]->State.HasFinished || (*this->Pawns)[3]->State.IsAtStart)) return;
+
+            //The player can do a move. Let the player choose until the player has chosen a correct pawn
             do 
             {
-                diceroll = Random::get<uint8_t>(1, 6) + Random::get<uint8_t>(1, 6);
-
-                LOG_I("Player %u rolls: %u", this->ID, diceroll);
-
                 selectedPawn = Random::get<uint8_t>(0, 3);
                 LOG_I("Player %u has selected pawn: %u", this->ID, selectedPawn);
                 LOG_I("Pawn %u is on board: %u", selectedPawn, !(*this->Pawns)[selectedPawn]->State.IsAtStart);
