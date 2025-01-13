73fff8f67216c7d17f73418419e7c7aa29cd5172
FReenen <git@finnvanreenen.nl>
2025-01-13 11:29:24 +0100

    Revert "Merge branch 'GameBase' into finley"
    This reverts commit b78abc6376bf0e780aa7862ad7835f057352b8bf, reversing
    changes made to d9252c9ecb821a13b9aded24d0eb4eee2f942a66.

--- MTG_Main/main/GameBase.hpp
+++ MTG_Main/main/GameBase.hpp
@@ -3,13 +3,6 @@
 #include "Player.hpp"
 #include "MotionController.hpp"
 #include "PathFinding.hpp"
-#include <stdio.h>
-#include <inttypes.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "nvs.h"
 
 template <class T, class U, class V>
 class GameBase {
@@ -23,5 +16,5 @@ class GameBase {
         V state;
         MotionController *motion;
         PathFinding_impoved *Pathfinding;
-        nvs_handle_t nvsFlashHandle;
+
 };
--- MTG_Main/main/ludoGame.hpp
+++ MTG_Main/main/ludoGame.hpp
@@ -12,5 +12,4 @@ class LudoGame : public GameBase<LudoPlayerState_s, LudoPawnState_t, LudoGameSta
 
     private:
         uint8_t currentPlayer;
-        void SaveGame();
 };
