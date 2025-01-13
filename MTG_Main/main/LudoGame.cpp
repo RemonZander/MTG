f9bb20b6c39df4349a02f0f0518c07e17c9b673a
remon zander <remon.zander1@gmail.com>
2025-01-13 14:48:33 +0100

    some more bugfixes

--- MTG_Main/main/LudoGame.cpp
+++ MTG_Main/main/LudoGame.cpp
@@ -325,10 +325,8 @@ void LudoGame::GameLoop()
                 //move pawn normally along the game path
                 else 
                 {
-                    LOG_I("moving pawn along board with steps: %u", (*(*this->players)[currentPlayer]->Pawns)[selectedPawn]->State.Steps);
                     if ((*(*this->players)[currentPlayer]->Pawns)[selectedPawn]->State.CurrentGamePath + (*(*this->players)[currentPlayer]->Pawns)[selectedPawn]->State.Steps > this->state.GamePath.size())
                     {
-                        LOG_I("Cycling back to the start of the gamepath");
                         (*(*this->players)[currentPlayer]->Pawns)[selectedPawn]->State.CurrentGamePath = (*(*this->players)[currentPlayer]->Pawns)[selectedPawn]->State.Steps - (this->state.GamePath.size() - (*(*this->players)[currentPlayer]->Pawns)[selectedPawn]->State.CurrentGamePath);
                     }
                     else 
@@ -349,6 +347,8 @@ void LudoGame::GameLoop()
                     this->state.allPawns[i]->squareCords.y, this->state.allPawns[i]->State.startPos.x, this->state.allPawns[i]->State.startPos.y);
                     this->motion->ExecutePath(this->Pathfinding->findPath(this->state.allPawns[i]->squareCords, this->state.allPawns[i]->State.startPos, this->map, &this->state.allPawns, 255));
                     this->state.allPawns[i]->squareCords = this->state.allPawns[i]->State.startPos;
+                    this->state.allPawns[i]->State.IsAtStart = true;
+                    this->state.allPawns[i]->State.IsInHome = false;
                     break;
                 }
             }
